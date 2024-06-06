967f73ce84b5b6aa34e35920004e28c9711e52d5
Jorge Cimentada <cimentadaj@gmail.com>
2024-06-06 13:18:52 +0200

    Adds setup.py to test install

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Define library metadata
+NAME = "honeycomb_gmaps"
+VERSION = "0.1.0"
+AUTHOR = "Jorge C"
+AUTHOR_EMAIL = "jorge@example.com"
+DESCRIPTION = "This is a short doc"
+
+# Dependencies (listed in requirements.txt)
+# (you don't need to specify them here)
+
+# Read requirements from requirements.txt
+with open("requirements.txt", "r") as f:
+  requirements = f.readlines()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    packages=find_packages(exclude=["tests*"]),  # Exclude test directories
+    install_requires=requirements,  # Use requirements from requirements.txt
+)
